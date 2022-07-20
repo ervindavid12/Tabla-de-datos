df504baac106e3a75f62d1339e8087f3930ac147
ervindavid12 <103067788+ervindavid12@users.noreply.github.com>
2022-07-19 19:15:59 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let user_number = 0;
+for (var i = 0; i>=7; i++)[
+   user_number++
+]
+
+import {user} from "./data.js"
+
+let root = document.getElementById("tablas");
+
+
+let listado = '<ol>' +
+                '<li>'+ user[user_number][0] + '</li>' + 
+             '</ol>';
+
+
+root.innerHTML = listado
